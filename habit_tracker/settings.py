2d5938e9b769b897c8b73cd1a44331e1d6b15468
James Andrews <jbandrew14@gmail.com>
2021-11-29 21:26:02 -0500

    edited BASE_DIR

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -16,10 +16,10 @@ import os
 import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 env = environ.Env(DEBUG=(bool, False))
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
