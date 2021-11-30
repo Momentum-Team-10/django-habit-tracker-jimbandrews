e15a6930fe7958966429a66b6a80cf53b100483a
James Andrews <jbandrew14@gmail.com>
2021-11-30 08:40:13 -0500

    created boilerplate for base.html, created User and Habit models, created notes.md to record coding process

---
+++ notes.md
@@ -0,0 +1,10 @@
+# Project Notes
+
+## Product Requirements
+
+1. registration and login  
+    - need to implement django-registration-redux
+
+2. what users should create
+    - start with User and Habit models
+    - used django [validator](https://docs.djangoproject.com/en/3.2/ref/validators/#minvaluevalidator) to put lower bound on Habit target
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class User(AbstractUser):
+    def __repr__(self):
+        return f"<User username={self.username}>"
+
+    def __str__(self):
+        return self.username
+
+
+class Habit(models.Model):
+    name = models.CharField(max_length=75)
+    target = models.IntegerField(validators=[MinValueValidator(0)])
