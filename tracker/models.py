98738039e757e1d24887ed33c760e67cac0007fb
James Andrews <jbandrew14@gmail.com>
2021-11-30 13:30:08 -0500

    added tracker to INSTALLED_APPS, added to Habit model and created DailyRecord model

--- tracker/models.py
+++ tracker/models.py
@@ -14,9 +14,24 @@ class User(AbstractUser):
 class Habit(models.Model):
     name = models.CharField(max_length=75)
     target = models.IntegerField(validators=[MinValueValidator(0)])
+    units = models.CharField(max_length=75)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
 
     def __repr__(self):
         return f"<Habit name={self.name}>"
 
     def __str__(self):
         return self.name
+
+
+class DailyRecord(models.Model):
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    habit_id = models.ForeignKey('Habit', on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f"<DailyRecord habit={self.habit_id}"
+
+    def __str__(self):
+        return self.habit_id
