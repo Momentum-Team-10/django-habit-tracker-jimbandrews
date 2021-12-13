e92a52cd3fb983395fb8f1ab859b0f2d327523b8
James Andrews <jbandrew14@gmail.com>
2021-12-13 16:49:13 -0500

    fixed djoser url paths

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -38,6 +38,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     # djoser
-    path('^auth/', include('djoser.urls')),
-    path('^auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
